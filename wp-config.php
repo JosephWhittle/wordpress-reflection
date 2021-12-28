ed31d0fc47e892572ecae40057745cdb1566b3a9
Joseph Whittle <Joewhittle101@gmail.com>
2021-12-28 12:50:10 +0000

    Bug fixes

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,9 @@
  * @package WordPress
  */
 
+  //ADDED THESE TO MANUALLY FIX THE LOG IN PROBLEMS
+  define('WP_SITEURL', 'http://localhost/wordpress-reflection/');
+  define('WP_HOME', 'http://localhost/wordpress-reflection/');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'wp_reflection' );
